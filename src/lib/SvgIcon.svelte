f8457df7e00fce3b9d2c5fa1fb67c6135cf0d59a
Russell Warner <rewarner@russellbits.com>
2022-11-26 16:43:43 -0500

    Adjusted SVGion component.

--- src/lib/SvgIcon.svelte
+++ src/lib/SvgIcon.svelte
@@ -6,8 +6,8 @@ export let d = ""
 <svg class="icon" width="24" height="24" viewBox="0 0 24 24" fill={fill} xmlns="http://www.w3.org/2000/svg">
   <path
     d={d}
-    stroke="#4A5568"
-    stroke-width="2"
+    stroke="white"
+    stroke-width="1"
     stroke-linecap="round"
     stroke-linejoin="round"/>
 </svg>
